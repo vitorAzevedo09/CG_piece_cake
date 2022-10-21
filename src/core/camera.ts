589f0b218d27b94a41e66c6e37dc24e761d1a8ab
Danilo Borges da Silva <daniloborges.cg@gmail.com>
2022-10-21 15:06:47 -0300

    separando funcionalidades de acordo com as responsabilidades

---
+++ src/core/camera.ts
@@ -0,0 +1,28 @@
+import { PerspectiveCamera } from "three"
+
+const VERTICAL_FIELD_OF_VIEW = 45
+const NEAR = 0.1
+const FAR = 1000
+
+export const sizes = {
+  width: window.innerWidth,
+  height: window.innerHeight,
+}
+
+export const camera = new PerspectiveCamera(
+  VERTICAL_FIELD_OF_VIEW,
+  sizes.width / sizes.height,
+  NEAR,
+  FAR
+)
+
+camera.position.set(9, 4, 9)
+
+window.addEventListener("resize", () => {
+  sizes.width = window.innerWidth
+  sizes.height = window.innerHeight
+  camera.aspect = sizes.width / sizes.height
+  camera.updateProjectionMatrix()
+})
+
+export default camera
--- src/main.ts
+++ src/main.ts
@@ -1,26 +1,10 @@
 import "./style.css"
 import * as THREE from "three"
-import { OrbitControls } from "three/examples/jsm/controls/OrbitControls.js"
+import "./core/controls"
+import { renderer, updateRenderer } from "./core/renderer"
+import { camera } from "./core/camera"
 
 const scene = new THREE.Scene()
-
-const VERTICAL_FIELD_OF_VIEW = 45
-const NEAR = 0.1
-const FAR = 1000
-
-const sizes = {
-  width: window.innerWidth,
-  height: window.innerHeight,
-}
-
-const camera = new THREE.PerspectiveCamera(
-  VERTICAL_FIELD_OF_VIEW,
-  sizes.width / sizes.height,
-  NEAR,
-  FAR
-)
-
-camera.position.set(9, 4, 9)
 
 const ambientLight = new THREE.AmbientLight(0xffffff, 0.5)
 scene.add(ambientLight)
@@ -33,31 +17,6 @@ directionalLight.shadow.normalBias = 0.05
 directionalLight.position.set(0.25, 2, 2.25)
 
 scene.add(directionalLight)
-
-const canvas: HTMLElement = document.querySelector("#experience") as HTMLElement
-
-const renderer = new THREE.WebGLRenderer({
-  canvas,
-})
-renderer.setSize(window.innerWidth, window.innerHeight)
-document.body.appendChild(renderer.domElement)
-
-renderer.shadowMap.enabled = true
-renderer.shadowMap.type = THREE.PCFShadowMap
-renderer.physicallyCorrectLights = true
-
-function updateRenderer() {
-  renderer.setSize(sizes.width, sizes.height)
-  renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
-}
-
-window.addEventListener("resize", () => {
-  sizes.width = window.innerWidth
-  sizes.height = window.innerHeight
-  updateRenderer()
-  camera.aspect = sizes.width / sizes.height
-  camera.updateProjectionMatrix()
-})
 
 const sphere = new THREE.Mesh(
   new THREE.SphereGeometry(1, 32, 32),
@@ -83,7 +42,6 @@ plane.rotation.set(-Math.PI / 2, 0, 0)
 scene.add(plane)
 
 updateRenderer()
-export const controls = new OrbitControls(camera, renderer.domElement)
 
 function animate() {
   requestAnimationFrame(animate)
