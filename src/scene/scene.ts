c3ee419f34041f85865a9fd59afe99fbbe686f18
Vitor Silva <vitor.azevedo009@gmail.com>
2022-10-31 17:06:30 -0300

    Deploy

--- src/scene/scene.ts
+++ src/scene/scene.ts
@@ -1,28 +1,22 @@
 import {
-  Scene,
-  AxesHelper,
-  AmbientLight,
-  DirectionalLight,
-  Mesh,
-  SphereGeometry,
-  MeshToonMaterial,
-  PlaneGeometry,
-  Color,
+    Scene,
+    AmbientLight,
+    DirectionalLight,
+    Mesh,
+    MeshBasicMaterial,
+    MeshToonMaterial,
+    PlaneGeometry,
+    Color,
+    BoxGeometry,
+    ConeGeometry,
+    CylinderGeometry,
+    CircleGeometry
 } from "three"
-import { renderer, updateRenderer } from "/src/core/renderer"
-
-import { gui } from "/src/core/gui"
+import {updateRenderer} from "../core/renderer"
 
 export const scene = new Scene()
 
-// Axes Helper
-const axesHelper = new AxesHelper(0.5)
-scene.add(axesHelper)
-
-gui.addInput(axesHelper, "visible", {
-  label: "AxesHelper",
-})
-
+// Light Code
 const ambientLight = new AmbientLight(0xffffff, 0.5)
 scene.add(ambientLight)
 
@@ -35,62 +29,174 @@ directionalLight.position.set(0.25, 2, 2.25)
 
 scene.add(directionalLight)
 
-const PARAMS = {
-  color: "#5EDCAE",
-}
-
-const sphere = new Mesh(
-  new SphereGeometry(0.75, 32, 32),
-  new MeshToonMaterial({
-    color: new Color(PARAMS.color),
-    wireframe: false,
-  })
-)
-
-sphere.position.set(0, 2, 0)
-sphere.castShadow = true
-
-const sphereCtrls = gui.addFolder({
-  title: "Sphere",
-})
-
-sphereCtrls.addInput(sphere.position, "x", {
-  label: "pos x",
-  min: -10,
-  max: 10,
-  step: 0.1,
-})
-sphereCtrls.addInput(sphere.position, "y", {
-  label: "pos y",
-  min: -10,
-  max: 10,
-  step: 0.1,
-})
-sphereCtrls.addInput(sphere.position, "z", {
-  label: "pos z",
-  min: -10,
-  max: 10,
-  step: 0.1,
-})
-sphereCtrls.addInput(PARAMS, "color").on("change", (e) => {
-  sphere.material.color = new Color(e.value)
-})
-
-sphereCtrls.addInput(sphere.material, "wireframe")
-
-scene.add(sphere)
-
+// Plane Code
 const plane = new Mesh(
-  new PlaneGeometry(10, 10, 10, 10),
-  new MeshToonMaterial({
-    color: new Color("#444"),
-  })
+    new PlaneGeometry(10, 10, 10, 10),
+    new MeshToonMaterial({
+        color: new Color("#444"),
+    })
 )
 
 plane.receiveShadow = true
 plane.rotation.set(-Math.PI / 2, 0, 0)
+
 scene.add(plane)
 
+// Plate Code
+const material = new MeshBasicMaterial({color: 0xffffff});
+
+const basePlate = new Mesh(
+    new BoxGeometry(1, 1, 0.5)
+    , material);
+
+const topPlate = new Mesh(
+    new ConeGeometry(2, 0.5, 32),
+    material);
+
+basePlate.rotation.set(-Math.PI / 2, 0, 0);
+basePlate.position.set(0, 0.2, 0);
+topPlate.rotation.set(Math.PI, 0, 0);
+topPlate.position.set(0, 0.2, 0);
+
+scene.add(basePlate);
+scene.add(topPlate);
+
+// Piece of cake code
+
+const cakeMaterial = new MeshBasicMaterial({color: 0x5c63c})
+
+const sideLeft = new Mesh(
+    new BoxGeometry(0.01, 0.5, 2.02),
+    cakeMaterial,
+)
+sideLeft.position.set(-0.3, 0.5, 0);
+sideLeft.rotation.set(0, 0.30, 0);
+
+const sideRight = new Mesh(
+    new BoxGeometry(0.01, 0.5, 2.02),
+    cakeMaterial,
+)
+sideRight.position.set(0.3, 0.5, 0);
+sideRight.rotation.set(0, -0.30, 0);
+
+const sideBack = new Mesh(
+    new CylinderGeometry(1.05, 1.05, 0.5, 32, 1, false, 2.54, 1.2),
+    cakeMaterial,
+);
+sideBack.position.set(0, 0.5, -0.1);
+sideBack.rotation.set(0, 0, 0);
+
+scene.add(sideRight);
+scene.add(sideLeft);
+scene.add(sideBack);
+
+
+// Piece of cake 1 code
+
+const cakeMaterial1 = new MeshBasicMaterial({color: 0xf47f6b})
+
+const sideLeft1 = new Mesh(
+    new BoxGeometry(0.01, 0.5, 2.02),
+    cakeMaterial1,
+)
+sideLeft1.position.set(-0.3, 1, 0);
+sideLeft1.rotation.set(0, 0.30, 0);
+
+const sideRight1 = new Mesh(
+    new BoxGeometry(0.01, 0.5, 2.02),
+    cakeMaterial1,
+)
+sideRight1.position.set(0.3, 1, 0);
+sideRight1.rotation.set(0, -0.30, 0);
+
+const sideBack1 = new Mesh(
+    new CylinderGeometry(1.05, 1.05, 0.5, 32, 1, false, 2.54, 1.2),
+    cakeMaterial1,
+);
+sideBack1.position.set(0, 1, -0.1);
+sideBack1.rotation.set(0, 0, 0);
+
+scene.add(sideRight1);
+scene.add(sideLeft1);
+scene.add(sideBack1);
+
+
+// Piece of cake 2 code
+
+const cakeMaterial2 = new MeshBasicMaterial({color: 0xbb5098})
+
+const sideLeft2 = new Mesh(
+    new BoxGeometry(0.01, 0.5, 2.02),
+    cakeMaterial2,
+)
+sideLeft2.position.set(-0.3, 1.5, 0);
+sideLeft2.rotation.set(0, 0.30, 0);
+
+const sideRight2 = new Mesh(
+    new BoxGeometry(0.01, 0.5, 2.02),
+    cakeMaterial2,
+)
+sideRight2.position.set(0.3, 1.5, 0);
+sideRight2.rotation.set(0, -0.30, 0);
+
+const sideBack2 = new Mesh(
+    new CylinderGeometry(1.05, 1.05, 0.5, 32, 1, false, 2.54, 1.2),
+    cakeMaterial2,
+);
+sideBack2.position.set(0, 1.5, -0.1);
+sideBack2.rotation.set(0, 0, 0);
+
+scene.add(sideRight2);
+scene.add(sideLeft2);
+scene.add(sideBack2);
+
+
+// Piece of cake 3 code
+
+const cakeMaterial3 = new MeshBasicMaterial({color: 0x7a5197})
+
+const sideLeft3 = new Mesh(
+    new BoxGeometry(0.01, 0.5, 2.02),
+    cakeMaterial3,
+)
+sideLeft3.position.set(-0.3, 2, 0);
+sideLeft3.rotation.set(0, 0.30, 0);
+
+const sideRight3 = new Mesh(
+    new BoxGeometry(0.01, 0.5, 2.02),
+    cakeMaterial3,
+)
+sideRight3.position.set(0.3, 2, 0);
+sideRight3.rotation.set(0, -0.30, 0);
+
+const sideBack3 = new Mesh(
+    new CylinderGeometry(1.05, 1.05, 0.5, 32, 1, false, 2.54, 1.2),
+    cakeMaterial3,
+);
+sideBack3.position.set(0, 2, -0.1);
+sideBack3.rotation.set(0, 0, 0);
+
+scene.add(sideRight3);
+scene.add(sideLeft3);
+scene.add(sideBack3);
+
+// The cake top
+
+const topCake = new Mesh(
+    new CircleGeometry(2, 32, 1.27, 0.6),
+    cakeMaterial3,
+)
+topCake.rotateX(-Math.PI / 2);
+topCake.position.set(0, 2.22, 0.96);
+
+scene.add(topCake);
+
 export function updateScene() {
-  updateRenderer()
+    updateRenderer()
 }
+
+
+
+
+
+
